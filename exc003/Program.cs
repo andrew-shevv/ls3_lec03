4ac5a8ad7cc7a77eae2699750aaa94cbce0bf327
Andrew <vishnyaishere@gmail.com>
2022-08-23 00:43:17 +0300

    exc003 cherries assorted

--- exc003/Program.cs
+++ exc003/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// minmax sort
+
+int[] assortedCherries = {2,5,1,7,3,6,9,4,8};
+
+void Print(int[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void minMaxSort(int[] array){
+    for (int i = 0; i < array.Length - 1; i++){
+        int minPos = i;
+        for (int j = i + 1; j < array.Length; j++){
+            if (array[j] < array[minPos]){
+                minPos = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[minPos];
+        array[minPos] = temp;
+    }
+}
+
+Print(assortedCherries);
+minMaxSort(assortedCherries);
+Print(assortedCherries);
